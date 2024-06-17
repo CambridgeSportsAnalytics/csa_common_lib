e7f018e1d7015310f81f05cdb73f45b43ad7343e
LWaienCSA <lwaien@csanalytics.io>
2024-06-17 11:57:42 -0400

    Prototype with extra case handling for npz vault validator

--- helpers/_vault.py
+++ helpers/_vault.py
@@ -6,7 +6,7 @@ from csa_common_lib.custom_class.vault_metadata import VaultMetadata
 from csa_common_lib.custom_class.prediction_options import PredictionOptions
 
 
-def validate_vault_npz_data(y, X, theta, yhat_details, metadata:VaultMetadata, options:PredictionOptions) -> bool:
+def validate_vault_npz_data(y, X, theta, yhat_details, Metadata:VaultMetadata, Options:PredictionOptions) -> bool:
     """Validates npz formatting before saving to .npz file. Ensures data is loaded cleanly.
 
     Args:
@@ -25,7 +25,7 @@ def validate_vault_npz_data(y, X, theta, yhat_details, metadata:VaultMetadata, o
         bool: flag describing if data is formatted correctly
     """
 
-    k = int(options.k)
+    k = int(Options.k)
 
     # Check that column dimensions match accross data 
     if len(theta) < 1:
@@ -33,10 +33,14 @@ def validate_vault_npz_data(y, X, theta, yhat_details, metadata:VaultMetadata, o
     if len(theta) > 0:
         if len(theta[0]) != k:
             raise ValueError("The number of variables in theta does not match k")
-    if len(metadata.x_labels) != k: 
+    if len(Metadata.x_labels) != k: 
         raise ValueError("The number of supplied X_labels does not match k")
     if len(y) != len(X):
         raise ValueError("Mismatch in # of observations between X and y (inputs)")
+    if not isinstance(Metadata.observations, list):
+        raise ValueError("Metadata.observations needs to be of type list. It is of type: ", str(type(Metadata.observations)))
+    if not isinstance(Metadata.y_metric, list):
+        raise ValueError("Metadata.y_metric needs to be of type list. It is of type: ", str(type(Metadata.y_metric)))
     
     # If not of the above formatting errors come up, return True.
     return True
