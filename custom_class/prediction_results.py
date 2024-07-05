712e8f9c2bc11cbd99fa9caa5dca19680e27efb7
LWaienCSA <lwaien@csanalytics.io>
2024-07-05 09:32:56 -0400

    added __repr__ method to results class that presents user will all available attributes in the results class

--- custom_class/prediction_results.py
+++ custom_class/prediction_results.py
@@ -58,3 +58,9 @@ class PredictionResults:
         for attr in dir(self):
             if not attr.startswith('__') and not callable(getattr(self, attr)):
                 print(f"{attr}: {getattr(self, attr)}")
+
+
+    def __repr__(self):
+        class_name = self.__class__.__name__
+        attributes = "\n".join(f"- {key}" for key, value in self.__dict__.items())
+        return f"Result Attributes:\n--------- \n{attributes}\n--------- "
