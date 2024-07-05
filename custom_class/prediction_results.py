5d2d43c27e1375e8b3c09e0973e7ac9afa90ff44
LWaienCSA <lwaien@csanalytics.io>
2024-07-05 09:36:14 -0400

    update to repr output

--- custom_class/prediction_results.py
+++ custom_class/prediction_results.py
@@ -63,4 +63,4 @@ class PredictionResults:
     def __repr__(self):
         class_name = self.__class__.__name__
         attributes = "\n".join(f"- {key}" for key, value in self.__dict__.items())
-        return f"Result Attributes:\n--------- \n{attributes}\n--------- "
+        return f"\nResults:\n--------- \n{attributes}\n--------- "
