2e52fb7e5f1a856a0f5c09999684a4087fd78807
LWaienCSA <lwaien@csanalytics.io>
2024-07-05 15:08:46 -0400

    Made prototype method for summary stats of results
    Oversimplified implementation of generating t_stats from our results class. Also includes a summy df return gives the user a well formatted output

--- custom_class/prediction_results.py
+++ custom_class/prediction_results.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import statsmodels.api as sm
 
 class PredictionResults:
     """
@@ -37,7 +38,8 @@ class PredictionResults:
                 print(f"{attr}: {getattr(self, attr)}")
 
 
-    def head(self):
+    def head(self, y_actuals=None):
+
         df = pd.DataFrame({
             'yhat': self.yhat,
             'y_linear': self.y_linear,
@@ -46,6 +48,38 @@ class PredictionResults:
             'agreement': self.agreement
             
         })
+
+        df['yhat_nonlin'] = df['yhat'] - df['y_linear']
+
+        # If y_actuals are supplied, we generate t-stats as well
+        if y_actuals is not None:
+            # Flatten ndarray and convert to regular array
+            if isinstance(y_actuals, np.ndarray):
+                y_actuals = y_actuals.flatten().tolist()
+
+            if isinstance(y_actuals, pd.Series):
+                y_actuals = y_actuals.astype(int).tolist()    
+
+            # Check dimensions to ensure y_actual matches
+            if len(y_actuals) != len(self.yhat) or len(y_actuals) != len(self.y_linear):
+                raise Exception("y_actual dimensions do not match y_hat and/or y_linear")
+            
+
+            #### PROTOTYPING Replace tstats lib call below with our own function that will go in the csa_common_lib.toolbox.stats module isntead
+            """
+            # Perform regression and calculate t-stats
+            X = sm.add_constant(df[['y_linear', 'yhat_nonlin']])
+
+            # Set y_actuals as a constant and model
+            model = sm.OLS(y_actuals, X)
+            results = model.fit()
+
+            # Retrieve t-stats
+            t_stats = results.tvalues
+
+            print("T-Statistics:")
+            print(t_stats)
+            """
 
         print(df.head())
         return df
@@ -58,7 +92,7 @@ class PredictionResults:
             print(f"  .{attribute}")
 
     
-    # TODO --> Change repr to display Davids core summary stats so the user can immediately assess the results before digging deeper. 
+    # TODO --> Change repr to be simpler
     def __repr__(self):
         class_name = self.__class__.__name__
         attributes = "\n".join(f"- {key}" for key, value in self.__dict__.items())
