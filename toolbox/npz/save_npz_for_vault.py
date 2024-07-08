b840123d212e4c9d2a53bdd2070aaf689d088ad1
LWaienCSA <lwaien@csanalytics.io>
2024-07-08 14:07:34 -0400

    added support for Results class parameter in vault save pipeline

--- toolbox/npz/save_npz_for_vault.py
+++ toolbox/npz/save_npz_for_vault.py
@@ -9,6 +9,7 @@ from .npz_io import save_to_npz
 # Import necessary csa_common_lib modules
 from csa_common_lib.custom_class.prediction_options import PredictionOptions
 from csa_common_lib.custom_class.vault_metadata import VaultMetadata
+from csa_common_lib.custom_class.prediction_results import PredictionResults
 from csa_common_lib.helpers._vault import validate_vault_npz_data
 from csa_common_lib.toolbox.classes.class_utils import class_obj_to_dict
 
@@ -34,7 +35,7 @@ def convert_ndarray_to_list(obj):
         return obj
 
 
-def save(api_key:str, filename:str, y, X, theta, yhat_details, Metadata:VaultMetadata, Options:PredictionOptions = PredictionOptions()):
+def save(api_key:str, filename:str, y, X, theta, Results:PredictionResults, Metadata:VaultMetadata, Options:PredictionOptions):
     """save_to_npz() wrapper for uploading to vault results. 
 
     Args:
@@ -42,7 +43,7 @@ def save(api_key:str, filename:str, y, X, theta, yhat_details, Metadata:VaultMet
         y : y input data
         X : X input data
         theta : theta input data
-        yhat_details : result data generated from a prediction
+        Results : PredictionResults() obj containing result data generated from a prediction
         Metadata (VaultMetadata): Custom object for storing supporting experiment data
         Options (PredictionOptions, optional): Custom object for storing optional prediction paramaters 
             Defaults to PredictionOptions().
@@ -51,6 +52,8 @@ def save(api_key:str, filename:str, y, X, theta, yhat_details, Metadata:VaultMet
         Status printout and .npz file
     """
 
+    # Set raw prediction data to a variable 
+    yhat_details = Results.raw_data
 
     # Package all the input fields in a dictionary
     inputs = {'y': y,'X':X,'theta':theta,'options':class_obj_to_dict(Options)['options']}
