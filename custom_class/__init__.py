aa7c401bb7665126c07e243a6bcb888d6f57f30c
LWaienCSA <lwaien@csanalytics.io>
2024-09-11 12:28:42 -0400

    adding lapack inversion generalization to psr

--- custom_class/__init__.py
+++ custom_class/__init__.py
@@ -2,6 +2,5 @@ from .prediction_options import PredictionOptions
 from .prediction_options import MaxFitOptions
 from .prediction_options import OptVarOptions
 
-from ...csanalytics_local.private_classes._vault_metadata import VaultMetadata
 
 from .prediction_results import PredictionResults
