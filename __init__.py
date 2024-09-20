28fe8b38560c4e6023a1ebc8e5ec451115b9ecc0
Cel Kulasekaran <ckulasekaran@users.noreply.github.com>
2024-09-20 14:23:56 -0400

    Updated csa_common_lib/__init__.py
    Explicitly declare Options classes so to provide visibility to users as opposed to wildcard imports. There will be more housekeeping (presentation) items coming like these.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,15 @@
+"""CSA Common Library
+Description of module should go here.
 
-from .custom_class import *
+
+"""
+
+
+# Options classes available for Optimal Variable Grid prediciton,
+# Max Fit prediction, and relevance-based prediction.
+from .custom_class.prediction_options import OptVarOptions
+from .custom_class.prediction_options import MaxFitOptions
+from .custom_class.prediction_options import PredictionOptions
+
 from .toolbox import *
 from .helpers import *
